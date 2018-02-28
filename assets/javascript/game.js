c85e27213dc0d64123aa4f381c01877e0e2c623a
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-02-28 16:44:38 -0600

    Calls a word at random and splits the string into its component parts.

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//I need an array that stores the list of words I want to be called for Hangman.
+//I chose cat for a word, moon for repeated letters, and zulu for the same letters not next to each other.
+var hangmanWords = ["CAT", "MOON", "ZULU"];
+//I need an array to store and display the letters guessed.
+var lettersGuessed = [];
+//I need a number to variable for wins and losses.
+var gameWins = 0;
+var gameLosses = 0;
+//I need an initialization function.
+function init() {
+    var lettersGuessed = [];
+}
+//I need to choose a random word from the array to be the Hangman word.
+function wordChoice() {
+return hangmanWords[Math.floor(Math.random()*hangmanWords.length)];
+}
+//I need blanks to be generated based on the number of letters in the word called for a game of Hangman.
+    //This returns an array in which the word chosen is split into component parts.
+var lettersToGuess = wordChoice().split("");
+console.log(lettersToGuess);
+//I need a function that listens for the users keypress.
+document.onkeypress = function(event) {
+
+}
+//The user guesses a letter.
+
+//The letter is added to the list of guessed letters.
+
+//If the letter is in the word, all instances of that letter are filled in.
+
+//If the letter is not in the word, a part is added to the hangman.
+
+//If the user guesses all of the letters, and the hangman is not complete, the user wins.
+
+//If the hangman is complete, and the user has not gotten all the letters, the user loses.
+
+//If neither has happened, the user may guess another letter.
+
+//If the user selects to play a new game, everything is reset except the wins/losses counters, and a new game starts.
+
+//If the user selects to quit, everything is reset.
