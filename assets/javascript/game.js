e2f8b6c9a72affab5e22aa602bb1bb43ba59dad2
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-02-28 18:07:13 -0600

    Blank Generation is working. Next up is check user input against computer input and replace blanks with items.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,40 @@
 //I need an array that stores the list of words I want to be called for Hangman.
 //I chose cat for a word, moon for repeated letters, and zulu for the same letters not next to each other.
-var hangmanWords = ["CAT", "MOON", "ZULU"];
+var hangmanWords = ["A", "CAT", "MOON", "ZULU"];
 //I need an array to store and display the letters guessed.
 var lettersGuessed = [];
 //I need a number to variable for wins and losses.
 var gameWins = 0;
 var gameLosses = 0;
+var blanksAndGuesses = [];
 //I need an initialization function.
 function init() {
     var lettersGuessed = [];
 }
 //I need to choose a random word from the array to be the Hangman word.
 function wordChoice() {
-return hangmanWords[Math.floor(Math.random()*hangmanWords.length)];
+    return hangmanWords[Math.floor(Math.random()*hangmanWords.length)];
 }
 //I need blanks to be generated based on the number of letters in the word called for a game of Hangman.
     //This returns an array in which the word chosen is split into component parts.
-var lettersToGuess = wordChoice().split("");
-console.log(lettersToGuess);
+function blankGeneration() {
+    var lettersToGuess = wordChoice().split("");
+    console.log(lettersToGuess);
+    for (let i = 0; i < lettersToGuess.length; i++) {
+
+        blanksAndGuesses.push("_ ");
+        }
+        document.getElementById("lettersToGuess").textContent = blanksAndGuesses.join(" ");    
+}
+console.log(blanksAndGuesses);
 //I need a function that listens for the users keypress.
 document.onkeypress = function(event) {
-
+    if (event.key) {
+        
+        
+    }
 }
+blankGeneration();
 //The user guesses a letter.
 
 //The letter is added to the list of guessed letters.
